720b2387f7e1e9c2f623a6e00f648d9738781f8d
Luis Fernando Padilla Almanza <lpadillaa@Grupo-Exito.com>
2019-01-29 15:28:02 -0500

    resolve warnings

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,6 +1,3 @@
-/**
- * This is a package.
- */
 package com.example;
 
 /**
@@ -15,8 +12,11 @@ public class Greeter {
 
   }
 
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
+  /**
+   * @param someone
+   * @return saludo
+   */
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
